0926f4505a8d33b224bf3a5ca1e8b11c2fed5714
EliamCS <angel-eliam@hotmail.com>
2026-03-10 10:56:21 -0600

    contador de clicks

--- main.dart
+++ main.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int clickCounter = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hola Mamawebo'));
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('Counter Screen'),
+      centerTitle: true,),
+      body: Center(child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Text('$clickCounter', style: TextStyle(
+          fontSize: 170,
+          fontWeight: FontWeight.w100),
+          ), 
+        Text('Clicks', style: TextStyle(fontSize: 25)),
+        ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
+        child: Icon(Icons.plus_one),
+      ),
+    )
+  );
   } 
 }
