a3d5623c77e97b64210b19fc2d594cdc14b1f221
Angel Eliam Contreras Santiago <L22350395@tuxtepec.tecnm.mx>
2026-03-20 11:12:25 -0600

    Segunda app de flutter

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://i.kym-cdn.com/entries/icons/newsfeed/000/055/742/mi-bombo.jpg'),
+          ),
+        ),
+        title: const Text('Mi Bombo!')
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            ],
+        ),
+      ),
+    );
+  }
+}
